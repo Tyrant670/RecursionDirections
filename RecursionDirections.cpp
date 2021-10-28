2d0ff19ce000edc264f7a8924b514ed5a8fd8d8c
Tyrant670 <88823371+Tyrant670@users.noreply.github.com>
2021-10-28 18:48:17 +0530

    Add files via upload

---
+++ RecursionDirections.cpp
@@ -0,0 +1,45 @@
+/**
+
+This Problem is to show How RECURSION works!
+              Check it Out !!!
+                Code it !
+
+**/
+
+/**
+ * Printing the numbers 1 to N using Recursion 
+ * Increasing and Decreasing
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Increasing(int n){
+    //Base Case..
+    if(n==0){
+        return;
+    }
+    //Recursive Case..
+    Increasing(n-1);
+    cout<<n<<" ";
+}
+
+void Decreasing(int n){
+    //Base Case..
+    if(n==0){
+        return;
+    }
+    //Recursive Case..
+    cout<<n<<" ";
+    Decreasing(n-1);
+}
+
+
+int main(){
+    cout<<"Enter the value of 'N' till you want to Print numbers :"<<endl;
+    int n; cin>>n;
+    //Increasing(n);
+    dec(n);
+
+    return 0;
+}
